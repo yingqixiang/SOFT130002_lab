68e7c025296ce7bd182d0ce86e5076fb5f90d97f
asdf <asdf>
2019-05-21 23:06:05 +0800

    lab8

---
+++ functions.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+    function outputOrderRow($file, $title, $quantity, $price) {
+        echo "<tr>";
+	$amount = $quantity*$price;
+	echo "<td><img src="images/books/tinysquare/$file"></td>";
+	echo "<td class="mdl-data-table__cell--non-numeric">$title</td>";
+	echo "<td>$quantity</td>";
+	echo "<td>$price</td>";
+	echo "<td>$amount</td>";
+        //TODO
+        echo "</tr>";
+    }
+?>
